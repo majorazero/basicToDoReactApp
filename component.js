313b092b743bbc231d824b916ca9ac7bae23efef
majorazero <daniel.cw.hsu@gmail.com>
2018-03-03 17:01:19 -0800

    added terrible styling and added some placeholders for future list functionality things

--- component.js
+++ component.js
@@ -1,25 +1,64 @@
- // let React = require('react');
- // let ReactDOM = require('react-dom');
-
 class Page extends React.Component {
-
   constructor (){
     super();
     this.state = {
-
       lists: []
     };
   }
-  ComponentWillMount(){
-    console.log(3);
-  }
   render(){
     return (
-      <div>Hello World!</div>
+    <div>
+      <h2> Your TO-DO List! </h2>
+      <div>
+        <List />
+        <ListForm />
+      </div>
+    </div>
     );
   }
+  //adds a list to an array of lists
+  _addList(listAuthor,listBody){
+    let list = {
+      author: listAuthor,
+      body: listBody,
+      id: this.state.lists.length+1
+    };
+    this.setState({
+      //will add the new list to the end of list object.
+      lists: this.state.lists.concat([list])
+    });
+  }
+}
 
+class List extends React.Component {
 
+  render(){
+    return(
+      <div>
+        <div> I am a list. </div>
+      </div>
+    );
+  }
+}
+
+class ListForm extends React.Component {
+
+  render(){
+    return(
+    <div>
+      <div className="listform-label">New List</div>
+      <div className="listform">
+        <div><input placeholder="List Title:"/></div>
+        <div><textarea placeholder="Description:"/></div>
+      </div>
+      <div>
+        <button type="submit">
+          Add List.
+        </button>
+      </div>
+    </div>
+    );
+  }
 }
 
 jQuery(function(){
